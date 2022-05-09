b021d97150fbe39ebb82b35dba88e45d1941d3d1
Geert Barentsen <hello@geert.io>
2022-05-09 09:41:23 -0700

    Update docs

---
+++ _downloads/0ef18905b0d9162865223a48a1d6e3d0/cloud-hosted-fits-files.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+==============================================
+Obtaining cutouts from FITS files in the cloud
+==============================================
+
+This example describes how to open a FITS file hosted in the cloud
+using `astropy.io.fits`.
+
+
+*By: Geert Barentsen*
+
+*License: BSD*
+
+
+"""
+
+from astropy.io import fits
+
+##############################################################################
+# TODO: Complete this example
